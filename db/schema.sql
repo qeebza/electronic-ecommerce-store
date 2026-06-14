58dd2c1df959b71411ec8498a782480ce9b38ab8
NgCong0215 <ng.jun.cong@student.mmu.edu.my>
2026-06-15 00:29:26 +0800

    schema.sql update(add username at users table)

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,7 @@ USE ecommerce_db;
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     phone VARCHAR(20),
